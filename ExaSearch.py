c34d8488438fcf92bc59a15f606dca620d41d422
AVidhanR <itsvidhanreddy@gmail.com>
2024-05-15 09:20:03 +0530

    Update ExaSearch.py

--- ExaSearch.py
+++ ExaSearch.py
@@ -1,20 +1,39 @@
 import streamlit as st
-import exa_py as Exa
+from exa_py import Exa
+import requests
+
+def display_content(title, url):
+    ai.markdown(f"""
+    <div style='padding: 2rem;'>
+        <h3> Title of the Search: {title} </h2>
+        <h4> URL to look for more: <a href="{url}" target = "_blank">Click here</a> </h3>
+    </div>
+    """,
+    unsafe_allow_html=True
+    )
+
+def get_wikipedia_page_content(title):
+    base_url = "https://en.wikipedia.org/w/api.php"
+
 
 container = st.container()
-container.header("Search Anything!")
-container.chat_message("user")
-chat_input = container.chat_input("Enter your search prompt here")
+container.header("Search from #Wikipedia #Google")
+prompt = container.chat_message("user")
+prompt.write("Enter something")
+exa = Exa('a1906239-50ab-4ff0-8f18-a4a3f5fde925')
+try:
+    response = exa.search(
+        container.chat_input('Enter your search prompt here'),
+        num_results=1,
+        type='keyword',
+        include_domains=['https://www.wikipedia.org']
+    )
 
-epy = Exa.api('a1906239-50ab-4ff0-8f18-a4a3f5fde925')
-response = epy.search(
-    chat_input,
-    num_results=5,
-    type='keyword',
-    include_domains=['https://www.wikipedia.org/', 'https://www.google.com']
-)
+    if response:
+        ai = container.chat_message("ai")
+        for result in response.results:
+            display_content(result.title, result.url)
+            ai.write(" ")
 
-for result in response.results:
-    container.write_stream(f'Title: {result.title}')
-    container.write_stream(f'URL: {result.url}')
-    container.write_stream()
+except Exception:
+    container.write("")
