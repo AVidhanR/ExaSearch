f14cf8974101b96852d7723eeb925418fcff3ba4
AVidhanR <itsvidhanreddy@gmail.com>
2024-05-15 08:23:00 +0530

    Base code initiated

--- ExaSearch.py
+++ ExaSearch.py
@@ -1,2 +1,7 @@
 import streamlit as st
 
+container = st.container()
+container.title("Search Anything!")
+container.write("Type in and search for something...")
+chat_input = container.chat_input("Enter your search prompt here")
+
